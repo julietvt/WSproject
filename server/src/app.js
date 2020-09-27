f09a245a99be4522851e68d9ae5d1716ee006606
julietvt <freshitpt@gmail.com>
2020-09-27 12:16:57 +0300

    WebApi: example chat

--- server/src/app.js
+++ server/src/app.js
@@ -1,11 +1,20 @@
 const express = require('express');
 const app = express();
 const expressWS = require('express-ws')(app);
+const gWss = expressWS.getWss('/');
+
 app.use(express.json());
 
+const messages = [];
+
 app.ws('/', function(ws, req) {
+    ws.send(JSON.stringify(messages));
     ws.on('message', function(msg) {
         console.log(msg);
+        messages.push(msg);
+        gWss.clients.forEach(function (client) {
+            client.send(JSON.stringify([msg]));
+        });
     });
     console.log('socket', req.testing);
 });
