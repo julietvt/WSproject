9a117686a47333c35954ae6dd862dccfc8ddefbb
julietvt <freshitpt@gmail.com>
2020-09-27 11:34:02 +0300

    WebApi example

--- server/src/app.js
+++ server/src/app.js
@@ -1,4 +1,14 @@
 const express = require('express');
+const app = express();
+const expressWS = require('express-ws')(app);
 app.use(express.json());
+
+app.ws('/', function(ws, req) {
+    ws.on('message', function(msg) {
+        console.log(msg);
+    });
+    console.log('socket', req.testing);
+});
+
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, ()=>console.log(`App lintening on port ${PORT}`));
